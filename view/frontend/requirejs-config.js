b77b560414046366013aeee3ef3e7412de92870e
Alex Dong <support@alothemes.com>
2020-07-21 10:35:09 +0700

    Add jquery.easing

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -8,10 +8,14 @@ var config = {
 
 	paths: {
 		'magiccart/slick'	: 'Magiccart_Community/js/plugin/slick.min',
+		'magiccart/easing'	: 'Magiccart_Community/js/plugin/jquery.easing.min'
 	},
-	
+
 	shim: {
 		'magiccart/slick': {
+			deps: ['jquery']
+		},
+		'magiccart/easing': {
 			deps: ['jquery']
 		}
 	}
